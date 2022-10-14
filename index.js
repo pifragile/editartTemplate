93e410321dac0c8c5e3d3bf0b0d73c366f6e79a9
pifragile <pigu@bluewin.ch>
2022-10-14 17:37:13 +0800

    introduce randomness

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ function setImage() {
 }
 
 function drawArt() {
-    // DRAW YOUR ART HERE
+    // DRAW YOUR ART HERE using p5js
     // using the variables m0, m1, m2, m3, m4
+    // every single part of the output must ONLY be dependent on the values m0..m4.
+    // if you need randomness, you can use the functions randomM0()..randomM4()
+    // these functions provide randomness seeded by the respective values m0..m4
 
     // Example:
     pg.noStroke();
     pg.background(m0 * 360, 100, 100);
     pg.fill(m1 * 360, 100, 100);
-    pg.circle(m2 * cs, m3 * cs, m4 * cs);
+    pg.circle(m2 * cs, m3 * cs, 0.2 * cs);
+    for (let i = 0; i < 200; i++) {
+        pg.circle(randomM4() * cs, randomM4() * cs, randomM4() * cs * 0.01);
+    }
+
 }
 function draw() {
     noLoop();
